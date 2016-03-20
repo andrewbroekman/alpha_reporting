c953d33fc19ceba154399d9a8dadbe43c750d785
Brandon Wardley <u29005150@tuks.co.za>
2016-03-21 00:09:39 +0200

    Added ReportingResponse.java
    Added the ReportingResponse class, which represents the object which
    will be returned once a ReportingRequest has been satisfied. Further
    attributes will be added once the format of the returned object has been
    finalised.

---
+++ src/main/java/com/codinginfinity/research/report/ReportingResponse.java
@@ -0,0 +1,13 @@
+/**
+*	@file ReportingResponse.java
+*	@author COS301 Reporting Alpha Team
+*	@version 1.0 alpha*
+*	@brief An object that represents a completed reporting request
+*	@section Description
+*	This object is what is returned once a reporting request is complete. It contains the generated reports based off of the data sent in the ReportingRequest object.
+*
+*/
+
+public class ReportingResponse {
+
+}
