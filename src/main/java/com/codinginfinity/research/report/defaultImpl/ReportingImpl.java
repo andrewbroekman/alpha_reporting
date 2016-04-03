f7d4ec411efbd893a4f9b5d0130190c126b89281
TshepoMalesela <u14211582@tuks.co.za>
2016-04-03 17:29:06 +0200

    new file for implementation

---
+++ src/main/java/com/codinginfinity/research/report/defaultImpl/ReportingImpl.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.codinginfinity.research.report.defaultImpl;
+
+import com.codinginfinity.research.report.GetAccreditationUnitReportRequest;
+import com.codinginfinity.research.report.GetAccreditationUnitReportResponse;
+import com.codinginfinity.research.report.GetProgressReportRequest;
+import com.codinginfinity.research.report.GetProgressReportResponse;
+import com.codinginfinity.research.report.InvalidRequestException;
+import com.codinginfinity.research.report.Reporting;
+
+/**
+ *
+ * @author Tshepo
+ */
+public class ReportingImpl implements Reporting{
+
+    @Override
+    public GetAccreditationUnitReportResponse getAccreditationUnitReport(GetAccreditationUnitReportRequest getAccreditationUnitReportRequest) throws InvalidRequestException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public GetProgressReportResponse getProgressReport(GetProgressReportRequest getProgressReportRequest) throws InvalidRequestException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
