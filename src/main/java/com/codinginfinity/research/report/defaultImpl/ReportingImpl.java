b96506be66ff46c0d477e1c67f823780cbeb52ac
TshepoMalesela <u14211582@tuks.co.za>
2016-04-05 17:01:37 +0200

    New Method - Helper function that helps build the queries

--- src/main/java/com/codinginfinity/research/report/defaultImpl/ReportingImpl.java
+++ src/main/java/com/codinginfinity/research/report/defaultImpl/ReportingImpl.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.codinginfinity.research.report.defaultImpl;
 
 import com.codinginfinity.research.report.GetAccreditationUnitReportRequest;
@@ -14,7 +9,7 @@ import com.codinginfinity.research.report.Reporting;
 
 /**
  *
- * @author Tshepo
+ * @author 
  */
 public class ReportingImpl implements Reporting{
 
@@ -26,6 +21,58 @@ public class ReportingImpl implements Reporting{
     @Override
     public GetProgressReportResponse getProgressReport(GetProgressReportRequest getProgressReportRequest) throws InvalidRequestException {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    /**
+     * 
+     * @param entity
+     * @param pubType
+     * @return Query This function returns the query string 
+     */
+    private String buildProgressReport( Entity entity, PublicationType pubType){
+        if(Entity == null)
+        {
+            
+            return "SELECT p.title as TITLE, p.misc as PROGRESS, r.name as RESEARCH_GROUP"
+                   +"FROM PUBLICATION p"
+                   +"INNER JOIN PUBLICATION_PERSON s ON p.publicationid = s.publication_publicationid  "
+                   + "INNER JOIN PERSON u ON u.personid = s.authors_personid"
+                   + "INNER JOIN RESEARCHGROUP r ON r.groupid = u.group_groupid"
+                   + "WHERE p.lifecyclestate = 'InProgress'" 
+                   + "AND p.name = '" + pubtype.getName() + "'"
+                   + "GROUP BY p.title" ;
+        }
+        else if(pubtype == null)
+        {
+            String type = Entity.getType();
+            String name = Entity.getName();
+            
+            if(type == "group"){
+                
+           return "SELECT p.title as TITLE, p.misc as PROGRESS, r.name as RESEARCH_GROUP"
+                   +"FROM PUBLICATION p"
+                   +"INNER JOIN PUBLICATION_PERSON s ON p.publicationid = s.publication_publicationid  "
+                   + "INNER JOIN PERSON u ON u.personid = s.authors_personid"
+                   + "INNER JOIN RESEARCHGROUP r ON r.groupid = u.group_groupid"
+                   + "WHERE p.lifecyclestate = 'InProgress'" 
+                   + "AND r.name = '" + name + "'"
+                   + "GROUP BY p.title" ;
+            }
+            else {
+                return "SELECT p.title as TITLE, p.misc as PROGRESS, r.name as RESEARCH_GROUP"
+                   +"FROM PUBLICATION p"
+                   +"INNER JOIN PUBLICATION_PERSON s ON p.publicationid = s.publication_publicationid  "
+                   + "INNER JOIN PERSON u ON u.personid = s.authors_personid"
+                   + "INNER JOIN RESEARCHGROUP r ON r.groupid = u.group_groupid"
+                   + "WHERE p.lifecyclestate = 'InProgress'" 
+                   + "AND u.firstnames = '" + name + "'"
+                   + "GROUP BY p.title" ;
+            }
+        }
+        
+        //TO-DO
+        //Finish implementation
+        return "";
     }
     
 }
