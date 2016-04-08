906a6c37946566633204ea209f24c0e90285f115
NsovoBaloyi <nsovo.baloyi23@gmail.com>
2016-04-08 17:36:47 +0200

    Made mock objects to be injected into the test class

--- src/test/java/com/codinginfinity/research/report/ReportingTestConfiguration.java
+++ src/test/java/com/codinginfinity/research/report/ReportingTestConfiguration.java
@@ -5,26 +5,57 @@
  */
 package com.codinginfinity.research.report;
 
+import com.codinginfinity.research.people.ReqEntity;
+import com.codinginfinity.research.publication.PublicationType;
 import com.codinginfinity.research.report.defaultImpl.ReportingImpl;
+import com.codinginfinity.research.report.defaultImpl.GetProgressReportRequest;
+import com.codinginfinity.research.report.defaultImpl.GetAccreditationUnitReportRequest;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 /**
  *
  * @author Nsovo
  */
+@Configuration
 class ReportingTestConfiguration {
-     @Bean
-    public ReportingImpl ReportingMock()
-    {
-        
+    @Bean
+    public ReportingImpl ReportingMock(){        
         return new ReportingImpl();
     }
     
-     @Bean
-    public GetProgressReportRequest RequestMock()
-    {
-        
-        return null;
+    /**
+     * 
+     * @return 
+     */
+    @Bean
+    public GetAccreditationUnitReportRequest ErrorAccUnitReportRequestMock(){        
+        return new GetAccreditationUnitReportRequest();
     }
+    
+    //============================================================================================
+    //============================================================================================
+    
+    /**
+     * 
+     * @return 
+     */
+    @Bean
+    public GetProgressReportRequest ErrorProgressReportRequestMock(){        
+        return new GetProgressReportRequest();
+    }
+    
+    /**
+     * 
+     * @return 
+     */
+    @Bean
+    public GetProgressReportRequest PersonEntityRequestMock()
+    {
+        ReqEntity e  = new ReqEntity("PERSON", "Dave");
+               
+        return new GetProgressReportRequest(e);
+    }
+    
     
 }
