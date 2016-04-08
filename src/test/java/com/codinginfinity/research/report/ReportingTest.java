c3f7b266dc9c4718a4599c5f142bee4af1a05545
NsovoBaloyi <nsovo.baloyi23@gmail.com>
2016-04-08 17:36:15 +0200

    Implemented two test function. TO-DO: IMplement the rest of the test functions

--- src/test/java/com/codinginfinity/research/report/ReportingTest.java
+++ src/test/java/com/codinginfinity/research/report/ReportingTest.java
@@ -10,6 +10,10 @@
 */
 package com.codinginfinity.research.report;
 
+import com.codinginfinity.research.report.defaultImpl.GetAccreditationUnitReportRequest;
+import com.codinginfinity.research.report.defaultImpl.GetAccreditationUnitReportResponse;
+import com.codinginfinity.research.report.defaultImpl.GetProgressReportRequest;
+import com.codinginfinity.research.report.defaultImpl.GetProgressReportResponse;
 import com.codinginfinity.research.report.defaultImpl.ReportingImpl;
 import javax.inject.Inject;
 import org.junit.After;
@@ -27,12 +31,34 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @ContextConfiguration(classes = {ReportingTestConfiguration.class})
 public class ReportingTest {
     
+    /**
+     * 
+     * @throws Exception 
+     */
+    @Test(expected = InvalidRequestException.class)
+    public void progressReportTest1() throws Exception{
+        ReportingMock.getProgressReport(ErrorProgressReportRequestMock);
+    }
+    
+    /**
+     * 
+     * @throws Exception 
+     */
     @Test
-    public void progressReportTest1() {
+    public void progressReportTest2() throws Exception{
+        progressReportRes = ReportingMock.getProgressReport(PersonEntityRequestMock);
+        assertTrue("SVG was printed",progressReportRes.getSVG());
+        assertTrue("PDF was printed",progressReportRes.getPDF());
+        assertTrue("HTML was printed", progressReportRes.getHTML());
+               
+    }
+    
+    /*@Test
+    public void progressReportTest1() throws Exception {
                
         
         
-        
+        res = ReportingMock.getProgressReport(ErrorRequestMock);
         assertTrue("SVG was printed",res.getSVG());
         assertTrue("PDF was printed",res.getPDF());
         assertTrue("HTML was printed", res.getHTML());
@@ -40,14 +66,27 @@ public class ReportingTest {
         
        
         
-    }
+    }*/
     
     @Inject
     private ReportingImpl ReportingMock;
+    //===========================================================================================
     @Inject
-    private GetProgressReportRequest RequestMock;
+    private GetProgressReportRequest ErrorProgressReportRequestMock;
+    @Inject
+    private GetProgressReportRequest PersonEntityRequestMock;
+    /*
+    TO-DO Define the other Mock objects for each test
+    */
+    //==========================================================================================
+    @Inject
+    private GetAccreditationUnitReportRequest ErrorAccUnitReportRequestMock;
+    /*
+    TO-DO Define the other Mock objects for each test
+    */
     
-    private GetProgressReportResponse res;
+    private GetProgressReportResponse progressReportRes;
+    private GetAccreditationUnitReportResponse AccUnitReporRes;
     
     
     
