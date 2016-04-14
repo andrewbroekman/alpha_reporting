3cfbfaf72c7a5cfe2e9bb3273ce3cde417a25723
Brandon Wardley <u29005150@tuks.co.za>
2016-04-14 11:33:28 +0200

    Update Populate.java
    Added more Person, Group, and Publication objects to the database.

--- src/test/java/com/codinginfinity/research/report/Populate.java
+++ src/test/java/com/codinginfinity/research/report/Populate.java
@@ -107,19 +107,108 @@ public class Populate {
 		person3.setPublications(null);
 		person3.setPrimaryEmail("sally@email.com");
 		person3.setOrganisation("University of Pretoria: Computer Science Department");
+		
+		//Person4
+		Person person4 = new Person();
+
+		person4.setFirstNames("Sammy");
+		person4.setSurname("TestGuy4");
+
+		person4.setGroup(testGroup2);
+		person4.setResearchGroupAssociationType("member");
+
+		dateSInput = "2016-06-06";
+		dateEInput = "2016-08-06";
+
+		try {
+			dS = tmp.parse(dateSInput);
+			dE = tmp.parse(dateEInput);
+		} catch (ParseException e) {
+			System.out.println("Unparseable.");
+		}
+
+		person4.setStartDate(dS);
+		person4.setEndDate(dE);
+		person4.setPublications(null);
+		person4.setPrimaryEmail("sammy@email.com");
+		person4.setOrganisation("University of Pretoria: Computer Science Department");
+
+		//Person5
+		Person person5 = new Person();
+
+		person5.setFirstNames("Jackson");
+		person5.setSurname("TestGuy5");
+
+		person5.setGroup(testGroup2);
+		person5.setResearchGroupAssociationType("member");
+
+		dateSInput = "2016-01-06";
+		dateEInput = "2016-08-06";
+
+		try {
+			dS = tmp.parse(dateSInput);
+			dE = tmp.parse(dateEInput);
+		} catch (ParseException e) {
+			System.out.println("Unparseable.");
+		}
+
+		person5.setStartDate(dS);
+		person5.setEndDate(dE);
+		person5.setPublications(null);
+		person5.setPrimaryEmail("jackson@email.com");
+		person5.setOrganisation("University of Pretoria: Computer Science Department");
+
+		//Person6
+		Person person6 = new Person();
+
+		person6.setFirstNames("Bob");
+		person6.setSurname("TestGuy6");
+
+		ResearchGroup testGroup3 = new ResearchGroup();
+		testGroup3.setName("Test Research Group 3");
+
+		entitymanager.persist(testGroup3);
+
+		person6.setGroup(testGroup3);
+		person6.setResearchGroupAssociationType("groupLeader");
+
+		dateSInput = "2015-06-06";
+		dateEInput = "2016-06-06";
+
+		try {
+			dS = tmp.parse(dateSInput);
+			dE = tmp.parse(dateEInput);
+		} catch (ParseException e) {
+			System.out.println("Unparseable.");
+		}
+
+		person6.setStartDate(dS);
+		person6.setEndDate(dE);
+		person6.setPublications(null);
+		person6.setPrimaryEmail("bob@email.com");
+		person6.setOrganisation("University of Pretoria: Computer Science Department");
 
 		entitymanager.persist(person1);
 		entitymanager.persist(person2);
 		entitymanager.persist(person3);
+		entitymanager.persist(person4);
+		entitymanager.persist(person5);
+		entitymanager.persist(person6);
 
 		//Creating Lists of Authors
 		List<Person> authors1 = new ArrayList<Person>();
 		List<Person> authors2 = new ArrayList<Person>();
+		List<Person> authors3 = new ArrayList<Person>();
+		List<Person> authors4 = new ArrayList<Person>();
 
 		authors1.add(person1);
 		authors1.add(person2);
 		authors2.add(person3);
-		//authors2.add(person2);
+		authors2.add(person4);
+		authors2.add(person5);
+		authors3.add(person6);
+		authors4.add(person3);
+		authors4.add(person5);
 
 		// Creating Publications
 
@@ -182,9 +271,102 @@ public class Populate {
 			System.out.println("Unparseable.");
 		}
 		publication2.setEnvisagedPublicationDate(pD);
+		
+		// Publication3
+
+		Publication publication3 = new Publication();
+
+		publication3.setTitle("Test Publication 3");
+		publication3.setAuthors(authors3);
+		publication3.setGroup(testGroup3);
+
+		LifeCycleState state3 = new LifeCycleState();
+		state3.setLifeCycleState("InProgress");
+		state3.setMisc("78");
+
+		publication3.setLifeCycleState(state3);
+
+		PublicationType pubType3 = new PublicationType();
+		pubType3.setName("High-Impact Journal");
+		pubType3.setAccreditationPoints(1.5);
+
+		publication3.setPublicationType(pubType3);
+
+		pubDate = "2016-04-01";
+		pD = new Date();
+
+		try {
+			pD = tmp.parse(dateSInput);
+		} catch (ParseException e) {
+			System.out.println("Unparseable.");
+		}
+		publication3.setEnvisagedPublicationDate(pD);
+
+		// Publication4
+
+		Publication publication4 = new Publication();
+
+		publication4.setTitle("Test Publication 4");
+		publication4.setAuthors(authors4);
+		publication4.setGroup(testGroup2);
+
+		LifeCycleState state4 = new LifeCycleState();
+		state4.setLifeCycleState("Published");
+		state4.setMisc("2016-03-15");
+
+		publication4.setLifeCycleState(state4);
+
+		PublicationType pubType4 = new PublicationType();
+		pubType4.setName("Accredited Book");
+		pubType4.setAccreditationPoints(2.8);
+
+		publication4.setPublicationType(pubType4);
+
+		pubDate = "2016-04-01";
+		pD = new Date();
+
+		try {
+			pD = tmp.parse(dateSInput);
+		} catch (ParseException e) {
+			System.out.println("Unparseable.");
+		}
+		publication4.setEnvisagedPublicationDate(pD);
+
+		// Publication5
+
+		Publication publication5 = new Publication();
+
+		publication5.setTitle("Test Publication 5");
+		publication5.setAuthors(authors1);
+		publication5.setGroup(testGroup1);
+
+		LifeCycleState state5 = new LifeCycleState();
+		state5.setLifeCycleState("InProgress");
+		state5.setMisc("85");
+
+		publication5.setLifeCycleState(state5);
+
+		PublicationType pubType5 = new PublicationType();
+		pubType5.setName("High-Impact Journal");
+		pubType5.setAccreditationPoints(1.5);
+
+		publication5.setPublicationType(pubType5);
+
+		pubDate = "2016-05-01";
+		pD = new Date();
+
+		try {
+			pD = tmp.parse(dateSInput);
+		} catch (ParseException e) {
+			System.out.println("Unparseable.");
+		}
+		publication5.setEnvisagedPublicationDate(pD);
 
 		entitymanager.persist(publication1);
 		entitymanager.persist(publication2);
+		entitymanager.persist(publication3);
+		entitymanager.persist(publication4);
+		entitymanager.persist(publication5);
 
 		entitymanager.getTransaction().commit();
 
