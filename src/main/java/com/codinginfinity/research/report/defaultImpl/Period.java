2c2a42ecd3271565778f59bb7d041a14baf99d94
MalulekiNyuswa <malunyz@yahoo.com>
2016-04-14 15:56:42 +0200

    Added java documentation to the Period.java file

--- src/main/java/com/codinginfinity/research/report/defaultImpl/Period.java
+++ src/main/java/com/codinginfinity/research/report/defaultImpl/Period.java
@@ -4,31 +4,51 @@ import java.util.Date;
 
 //Mock object for the Period object used in com.codinginfinity.research.publication
 public class Period{
+        
+    /**
+     * This is a constructor with start and end dates.
+     * @param startD
+     * @param endD 
+     */
+    public Period(Date startD,Date endD)
+    {
+            startDate=startD;
+            endDate=endD;
+    }
 
-	public Period(Date startD,Date endD)
-	{
-		startDate=startD;
-		endDate=endD;
-	}
+    // startDate and endDate represent the lower and upper limit of the Period object, respectively
+    private Date startDate;
 
-	// startDate and endDate represent the lower and upper limit of the Period object, respectively
-	private Date startDate;
-
-	private Date endDate;
-
-	public Date getStartDate() {
-		return startDate;
-	}
-
-	public void setStartDate(Date s) {
-		this.startDate = s;
-	}
-
-	public Date getEndDate() {
-		return endDate;
-	}
-
-	public void setEndDate(Date e) {
-		this.endDate = e;
-	}
+    private Date endDate;
+    
+    /**
+     * This is a getter function for getting startDate object.
+     * @return Date
+     */
+    public Date getStartDate() {
+            return startDate;
+    }
+    
+    /**
+     * This a setter function for setting a startDate object.
+     * @param s 
+     */
+    public void setStartDate(Date s) {
+            this.startDate = s;
+    }
+    
+    /**
+     * This is a getter function for getting the end date.
+     * @return Date
+     */
+    public Date getEndDate() {
+            return endDate;
+    }
+    /**
+     * This is a setter function for setting the endDate object.
+     * @param e 
+     */
+    public void setEndDate(Date e) {
+            this.endDate = e;
+    }
 }
